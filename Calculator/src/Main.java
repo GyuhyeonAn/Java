fea10262e4d2535092eae1e7f9dc7bd882ffa4f5
GyuhyeonAn <gyuhyeon0035@naver.com>
2025-03-24 14:45:30 +0900

    revert: 4365f9a2c5127ef814d8ef1812f99c5d8d874e52

--- Calculator/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        double round = 10.0;
-        final double PI = 3.14;
-        System.out.println("반지름이 " + round + "인 원의 면적은 " + round*round*PI + "이다.");
-    }
-}
--- name/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String name = "안규현";
-        int age = 23;
-
-        System.out.println(name + "님은 " + age +"살 입니다");
-    }
-}
