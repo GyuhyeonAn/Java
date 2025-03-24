3fb4a8b2f695f66b8f24f82974a7bcd8365a63c3
GyuhyeonAn <gyuhyeon0035@naver.com>
2025-03-24 14:45:48 +0900

    revert: fea10262e4d2535092eae1e7f9dc7bd882ffa4f5

---
+++ name/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "안규현";
+        int age = 23;
+
+        System.out.println(name + "님은 " + age +"살 입니다");
+    }
+}
