7ac1185b94c6a66f82ab08e343b2a1f0358b70e8
GyuhyeonAn <gyuhyeon0035@naver.com>
2025-03-27 14:32:21 +0900

    형변환

---
+++ TypeCasting/src/TypeCastingCalculator.java
@@ -0,0 +1,12 @@
+public class TypeCastingCalculator {
+    public static void main(String[] args) {
+        double doublevalue = 2.71828;
+
+        System.out.println("\n=== double 형변환 ===");
+        System.out.println("1. double -> btye: " +(byte)doublevalue);
+        System.out.println("2. double -> short: " +(short)doublevalue);
+        System.out.println("3. double -> int: " +(int)doublevalue);
+        System.out.println("4. double -> long: " +(long)doublevalue);
+        System.out.println("5. double -> float: " +(float)doublevalue);
+    }
+}
