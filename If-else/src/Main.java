e2c7d9b1c1d15b7a0b22e4f3975d96754cac3da5
GyuhyeonAn <gyuhyeon0035@naver.com>
2025-04-14 14:40:03 +0900

    Update Main.java

--- If-else/src/Main.java
+++ If-else/src/Main.java
@@ -1,20 +1,44 @@
 import java.util.Scanner;
 
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String menu, size, option;
+        int menu, size, option, total = 0;
 
-        System.out.println("메뉴 : 아메리카노 | 카페라떼 | 카푸치노" + "\n" + "크기 : Small | Medium | large" + "\n" + "옵션 : 기본 | 샷 추가 | 시럽 추가 | 샷과 시럽 모두 추가");
-        System.out.print("메뉴를 입력해주세요. : ");
-        menu = sc.nextLine();
-        System.out.print("사이즈를 선택하세요. : ");
-        size = sc.nextLine();
-        System.out.print("옵션을 선택하세요. : ");
-        option = sc.nextLine();
+        System.out.print("메뉴를 입력해주세요. 아메리카노-1 카페라떼-2 카푸치노-3 : ");
+        menu = sc.nextInt();
+        System.out.print("사이즈를 선택하세요. 1-Small 2-Medium, 3-Large : ");
+        size = sc.nextInt();
+        System.out.print("옵션을 선택하세요 1-기본 2-샷추가 3-시럽추가 4-샷+시럽추가 : ");
+        option = sc.nextInt();
 
-        System.out.println(menu + " " + size + " " + option);
+        if (menu == 1) {
+            total += 3000;
+        }
+        else if (menu == 2) {
+            total += 4000;
+        }
+        else {
+            total += 4500;
+        }
+
+        if (size == 2) {
+            total += 500;
+        }
+        else if (size == 3) {
+            total += 1000;
+        }
+
+        if (option == 2) {
+            total += 500;
+        }
+        else if (option == 3) {
+            total += 300;
+        }
+        else if (option == 4) {
+            total += 800;
+        }
+
+        System.out.println("최종 가격은 " +total +"입니다.");
     }
 }
