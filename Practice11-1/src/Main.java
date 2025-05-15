50ed5eb02ea1886c9a0f2a5b86d0a246f764ee1b
GyuhyeonAn <gyuhyeon0035@naver.com>
2025-05-15 14:39:09 +0900

    5.15

---
+++ Practice11-1/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        while (true) {
+            System.out.print("1부터 100사이의 값을 입력하세요: ");
+            num = sc.nextInt();
+
+            if (num >= 1 && num <= 100) {
+                break;
+            }
+            else { System.out.println("잘못된 값 입력했습니다."); }
+        }
+        System.out.println("입력완료! 프로그램을 종료합니다. ");
+    }
+}
--- practice_10_2/src/Main.java
+++ practice_10_2/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int sum = 0, num;
 
-        System.out.print("정수를 입력하세요: ");
+        System.out.print("숫자를 입력하세요: ");
         num = sc.nextInt();
 
         for (int i = 1; i <= num; i ++) {
